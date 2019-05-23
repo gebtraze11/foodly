224baf5eb46b744530ff7cd22ea43e1add4f4985
gebtraze11 <gebtraze11@gmail.com>
2019-05-23 16:53:30 -0700

    renamed app.js, modify refrence

--- server.js
+++ server.js
@@ -2,11 +2,19 @@ var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
+var session = require('express-session');
+var passport = require('passport');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+
+require('dotenv').config();
+// require db
+
+require('./config/database')
+require('./config/passport')
 var app = express();
 
 // view engine setup
@@ -17,6 +25,13 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(session({
+  secret:'Foodly',
+  resave: false,
+  saveUninitialized: true
+})); 
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
