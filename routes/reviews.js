97b3251461319f80e712870fdf3e481ebe619897
gebtraze11 <gebtraze11@gmail.com>
2019-05-30 14:09:08 -0700

    first page styling

--- routes/reviews.js
+++ routes/reviews.js
@@ -3,7 +3,7 @@ var router = express.Router();
 var reviewsCtrl = require('../controllers/reviews')
 
 
-router.post('/food/:id', reviewsCtrl.create)
+router.post('/food/:id/reviews', reviewsCtrl.create)
 router.put('/reviews/:foodId/:reviewId', reviewsCtrl.update)
 router.delete('/reviews/:foodId/:reviewId', reviewsCtrl.deleteReview)
 
